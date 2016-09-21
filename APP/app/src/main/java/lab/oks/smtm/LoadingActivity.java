d63422aac28f5e836193b6784ef2bdc363bfa88f
CHOIUK <evollove@nate.com>
2016-09-21 19:08:36 +0900

    Loading Page update

--- APP/app/src/main/java/lab/oks/smtm/LoadingActivity.java
+++ APP/app/src/main/java/lab/oks/smtm/LoadingActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 public class LoadingActivity extends AppCompatActivity {
 
     private Button Btn1;
@@ -14,7 +16,9 @@ public class LoadingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_loading);
+
 
         Btn1 = (Button) findViewById(R.id.button);
         Btn2 = (Button) findViewById(R.id.button2);
--- APP/app/src/main/java/lab/oks/smtm/MenuViewActivity.java
+++ APP/app/src/main/java/lab/oks/smtm/MenuViewActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
@@ -17,28 +19,18 @@ public class MenuViewActivity extends AppCompatActivity {
     ViewPager pager;
     Button currencyBtn;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_view);
         getSupportActionBar().setTitle("맛집이름");
         pager = (ViewPager)findViewById(R.id.pager);
-        //ViewPager에 설정할 Adapter 객체 생성
-        //ListView에서 사용하는 Adapter와 같은 역할.
-        //다만. ViewPager로 스크롤 될 수 있도록 되어 있다는 것이 다름
-        //PagerAdapter를 상속받은 CustomAdapter 객체 생성
-        //CustomAdapter에게 LayoutInflater 객체 전달
+
         ImageCustomAdapter adapter= new ImageCustomAdapter(getLayoutInflater());
-        //ViewPager에 Adapter 설정
         pager.setAdapter(adapter);
-
-
 
         //RadioGroup azione = (RadioGroup) findViewById(R.id.radiogroup_menu);
         currencyBtn = (Button) findViewById(R.id.view_currencyBtn);
-
-
         currencyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,5 +49,8 @@ public class MenuViewActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 
 }
